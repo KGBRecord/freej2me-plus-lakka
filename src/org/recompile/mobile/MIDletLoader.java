c01bb47aace73d42cb28a5615e20796f487498ea
AShiningRay <genesis45.ppc@gmail.com>
2025-06-26 18:19:13 -0300

    MIDletLoader: Check if the "MicroEdition-Profile" key is present for KDDI
    Trying to get this key unconditionally would cause issues with DoJa, and
    any other jar that doesn't have this key present in the properties map
    at this stage, so check for the key's presence before trying to read
    from it.

--- src/org/recompile/mobile/MIDletLoader.java
+++ src/org/recompile/mobile/MIDletLoader.java
@@ -450,9 +450,10 @@ public class MIDletLoader extends URLClassLoader
 		}
 		else { Mobile.isDoJa = properties.containsKey("MIDlet-1") ? false : true; } // Else we assume it as DoJa if a JAD file wasn't found, or if it was found but it, like the manifest, doesn't have the MIDlet token
 
-		String microeditionProfile = properties.get("MicroEdition-Profile");
-		if (microeditionProfile.startsWith("KDDIP")) {
-			Mobile.isKDDI = true;
+		if(properties.containsKey("MicroEdition-Profile")) 
+		{
+			String microeditionProfile = properties.get("MicroEdition-Profile");
+			if (microeditionProfile.startsWith("KDDIP")) { Mobile.isKDDI = true; }
 		}
 
 		if(Mobile.isDoJa) // No manifest found in the jar, or the manifest doesn't have a midlet specified. Maybe it's a DoJa file that has an accompanying .jam?
