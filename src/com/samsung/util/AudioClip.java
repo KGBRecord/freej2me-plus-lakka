2cfd68c55eb873c0a17a9b3073b96b8fc4e9129c
AShiningRay <genesis45.ppc@gmail.com>
2025-07-12 17:45:54 -0300

    AudioClip: Improve volume and looping for SMAF files.
    It seems TYPE_MMF makes AudioClip looping and volume work just
    like MIDP2's Player.

--- src/com/samsung/util/AudioClip.java
+++ src/com/samsung/util/AudioClip.java
@@ -84,15 +84,16 @@ public class AudioClip
 
 	public void play(int loop, int volume) 
 	{
+		Mobile.log(Mobile.LOG_ERROR, AudioClip.class.getPackage().getName() + "." + AudioClip.class.getSimpleName() + ": " + "loop:" + loop + " vol:" + volume);
 		// MMF apparently accepts looping to -1 in AudioClip. Not stated on the documentation, but some jars like ClickMan use it specifically for MMF
-		if(loop < ((playerFormat == TYPE_MMF) ? -1 : 0) || loop > 255 || volume < 0 || volume > 5) { throw new IllegalArgumentException("AudioClip: Cannot play() media, invalid argument provided"); }
+		if(loop < ((playerFormat == TYPE_MMF) ? -1 : 0) || loop > 255 || volume < 0 || volume > ((playerFormat == TYPE_MMF) ? 100 : 5)) { throw new IllegalArgumentException("AudioClip: Cannot play() media, invalid argument provided"); }
 
 		try
 		{
 			if (player.getState() == Player.STARTED) { player.stop(); }
 			player.setMediaTime(0); // play() should always play media from the beginning, like Nokia Sound
-			player.setLoopCount((loop == -1 || loop == 255) ? -1 : loop+1); // Treat -1 loops, and the max allowed value as infinite looping
-			((VolumeControl) player.getControl("VolumeControl")).setLevel(volume * 20); // Received volume varies from 1 to 5, so adapt
+			player.setLoopCount((playerFormat == TYPE_MMF) ? loop : (loop == 255 ? -1 : loop+1)); // For non-MMF, treat 255 loops as infinite looping
+			((VolumeControl) player.getControl("VolumeControl")).setLevel((playerFormat == TYPE_MMF) ? volume : volume * 20); // Received volume varies from 1 to 5, so adapt
 			player.start();
 		}
 		catch (Exception e) {Mobile.log(Mobile.LOG_ERROR, AudioClip.class.getPackage().getName() + "." + AudioClip.class.getSimpleName() + ": " + "AudioClip: Failed to play():" + e.getMessage()); }
