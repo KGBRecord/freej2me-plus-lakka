KGBRecord/freej2me-plus-lakka

commit c546b1dbfbf544b2dce1f965743bb97d5e887d44

More large improvements to DoJa and Graphics, some reorganizations
Too much stuff to explain in detail, but the gist of it is that DoJa
now has its 5.1 classes aside from 3D libraries present, PlatformGraphics
and LCDUI received more noticeable improvements and optimizations (LCDUI
finally has DateField implemented), and file sources were organized to
make building more concise, and also faster by only rebuilding files
that truly changed since the last.

DoJa can also receive CLR inputs now, and LCDUI navigation for layouts
like siemens, skt, etc was also fixed... command issuing in general
was also improved. Other than that, minor issues here and there were
fixed as well.

Fixes #37
Fixes #59
Fixes #113
Fixes #135
Related to #45 (i think only the rotation feature is left now)

src/javax/microedition/lcdui/Displayable.java
@@ -194,9 +194,12 @@ public abstract class Displayable
 			xPos = (width / 4) - textCenter;
 			graphics.drawString("Okay", xPos, currentY+titlePadding, Graphics.LEFT);
 
-			textCenter = (graphics.getGraphics2D().getFontMetrics().stringWidth("Back"))/2;
-			xPos = (3 * width / 4) - textCenter;
-			graphics.drawString("Back", xPos, currentY+titlePadding, Graphics.LEFT);
+			if(hasBackCommand()) 
+			{
+				textCenter = (graphics.getGraphics2D().getFontMetrics().stringWidth("Back"))/2;
+				xPos = (3 * width / 4) - textCenter;
+				graphics.drawString("Back", xPos, currentY+titlePadding, Graphics.LEFT);
+			}
 		}
 		else // Render Items
 		{
@@ -284,12 +287,10 @@ public abstract class Displayable
 			doCommand(currentCommand);
 			listCommands = false;
 		}
-		else
+		else if(commands.size()>0 && commands.size()<=2)
 		{
-			if(commands.size()>0 && commands.size()<=2)
-			{
-				doCommand(0);
-			}
+			doCommand(0);
+			currentCommand = 0;
 		}
 	}
 
@@ -297,7 +298,15 @@ public abstract class Displayable
 	{
 		if(commands.size()>1 && commands.size()<=2)
 		{
-			doCommand(1);
+			for(int i = 0; i < commands.size(); i++) 
+			{
+				if(commands.get(i).getCommandType() == Command.BACK) 
+				{ 
+					doCommand(i); 
+					currentCommand = 0;
+					return; 
+				}
+			}
 		}
 	}
 
@@ -312,4 +321,13 @@ public abstract class Displayable
 		}); 
 	}
 
+	public boolean hasBackCommand() 
+	{
+		for(int i = 0; i < commands.size(); i++) 
+		{
+			if(commands.get(i).getCommandType() == Command.BACK) { return true; }
+		}
+
+		return false;
+	}
 }

src/org/recompile/freej2me/Libretro.java
@@ -505,8 +505,8 @@ public class Libretro
 								case 15:
 
 									// Check if the frontend is fast-forwarding
-									if(din[4] == 0) { MobilePlatform.pressedKeys[19] = false; }
-									else { MobilePlatform.pressedKeys[19] = true; }
+									if(din[4] == 0) { MobilePlatform.pressedKeys[20] = false; }
+									else { MobilePlatform.pressedKeys[20] = true; }
 
 									/* Send Frame to Libretro */
 									try

src/org/recompile/mobile/MobilePlatform.java
@@ -489,6 +489,10 @@ public class MobilePlatform
 				mask = 1 << 0x16;
 				eventKey = com.nttdocomo.ui.Display.KEY_SOFT2;
 				break;
+			case Mobile.KDDI_CLR:
+				mask = 1 << 0x20;
+				eventKey = com.nttdocomo.ui.Display.KEY_CLEAR;
+				break;
 			default:
 				mask = 0;
 		}
@@ -546,14 +550,12 @@ public class MobilePlatform
 				{
 					showCommandBar();
 					displayable.doLeftCommand();
-					displayable.currentCommand = 0;
 					return true;
 				}
 				else if (key == Canvas.KEY_SOFT_RIGHT) 
 				{
 					showCommandBar();
 					displayable.doRightCommand();
-					displayable.currentCommand = 0;
 					return true;
 				}
 			}

commit 74293734ac0938eda1ce981078f925db61c6001e

EXPERIMENTAL: General improvements and cleanups.
We don't seem to really need to forcibly set the encoding as UTF-8
or anything else when parsing paths, as FreeJ2ME-Plus already has
the capability to self-restart with the correct one once it finds
out that the application is meant to run in a korean or japanese
encoding format.

Also, Libretro now updates its settings properly, and right softkey
commands are issued as they should now, fixing a recent regression
caused by c546b1dbfbf544b2dce1f965743bb97d5e887d44.

src/javax/microedition/lcdui/Displayable.java
@@ -298,9 +298,14 @@ public abstract class Displayable
 	{
 		if(commands.size()>1 && commands.size()<=2)
 		{
+			doCommand(1); 
+			currentCommand = 0;
+		}
+		else if(commands.size() > 2)
+		{
 			for(int i = 0; i < commands.size(); i++) 
 			{
-				if(commands.get(i).getCommandType() == Command.BACK) 
+				if(commands.get(i).getCommandType() == Command.BACK) // Find the back command
 				{ 
 					doCommand(i); 
 					currentCommand = 0;

src/org/recompile/freej2me/Libretro.java
@@ -318,7 +318,7 @@ public class Libretro
 									buffer = new byte[code];
 									bytesRead = System.in.read(buffer);
 
-									path = new String(buffer, 0, bytesRead, "UTF-8");
+									path = new String(buffer, 0, bytesRead);
 
 									if(Mobile.getPlatform().load(getFormattedLocation(URLDecoder.decode(path.toString(), Mobile.textEncoding))))
 									{
@@ -425,7 +425,7 @@ public class Libretro
 									buffer = new byte[code];
 									bytesRead = System.in.read(buffer);
 
-									Mobile.getPlatform().dataPath = new String(buffer, 0, bytesRead, "UTF-8");
+									Mobile.getPlatform().dataPath = new String(buffer, 0, bytesRead);
 								break;
 
 								case 13:
@@ -433,7 +433,7 @@ public class Libretro
 									buffer = new byte[code];
 									bytesRead = System.in.read(buffer);
 									
-									String cfgvars = new String(buffer, 0, bytesRead, "UTF-8");
+									String cfgvars = new String(buffer, 0, bytesRead);
 									/* Tokens: [0]="FJ2ME_LR_OPTS:", [1]=width, [2]=height, [3]=rotate, [4]=phone, [5]=fps, ... */
 									cfgtokens = cfgvars.split("[| x]", 0);
 									/* 
@@ -458,7 +458,7 @@ public class Libretro
 									if(Integer.parseInt(cfgtokens[4])==9)  { Mobile.config.settings.put("phone", "SKT"); }
 									if(Integer.parseInt(cfgtokens[4])==10) { Mobile.config.settings.put("phone", "KDDI"); }
 
-									Mobile.config.settings.put("fps", ""+cfgtokens[5]);
+									Mobile.config.settings.put("fps", ""+ Integer.parseInt(cfgtokens[5]));
 
 									if(Integer.parseInt(cfgtokens[6])==1) { Mobile.config.settings.put("sound", "on");  }
 									if(Integer.parseInt(cfgtokens[6])==0) { Mobile.config.settings.put("sound", "off"); }

src/org/recompile/mobile/MIDletLoader.java
@@ -400,14 +400,14 @@ public class MIDletLoader extends URLClassLoader
 		}
 	}
 
-	public static void parseDescriptorInto(InputStream is, Map<String, String> keyValueMap, String charset)
+	public static void parseDescriptorInto(InputStream is, Map<String, String> keyValueMap)
 	{
 		boolean hasMIDlet = false;
         String currentKey = null;
         StringBuilder currentValue = new StringBuilder();
         try
 		{
-			BufferedReader br = new BufferedReader(new InputStreamReader(is, charset));
+			BufferedReader br = new BufferedReader(new InputStreamReader(is));
 			try 
 			{
 				String line;
@@ -469,7 +469,7 @@ public class MIDletLoader extends URLClassLoader
 	
 		try
 		{
-			BufferedReader br = new BufferedReader(new InputStreamReader(is, "Shift_JIS")); // DoJa uses the Shift_JIS charset in its descriptor
+			BufferedReader br = new BufferedReader(new InputStreamReader(is));
 			String line;
 			while ((line = br.readLine()) != null) 
 			{
@@ -521,7 +521,7 @@ public class MIDletLoader extends URLClassLoader
 
 		if(url != null) // Standard MIDlet manifest is present (at least i assume so)
 		{
-			try { parseDescriptorInto(url.openStream(), properties, "UTF-8"); }
+			try { parseDescriptorInto(url.openStream(), properties); }
 			catch (Exception e) 
 			{
 				Mobile.log(Mobile.LOG_ERROR, MIDletLoader.class.getPackage().getName() + "." + MIDletLoader.class.getSimpleName() + ": " + "Can't Read Jar Manifest!");

src/org/recompile/mobile/MobilePlatform.java
@@ -658,7 +658,7 @@ public class MobilePlatform
 				try
 				{
 					InputStream targetStream = new FileInputStream(tmpfile);
-					MIDletLoader.parseDescriptorInto(targetStream, descriptorProperties, "UTF-8");
+					MIDletLoader.parseDescriptorInto(targetStream, descriptorProperties);
 					targetStream.close();
 				}
 				catch (IOException e) 
@@ -726,17 +726,15 @@ public class MobilePlatform
 
 			if (isJad || isMsd) 
 			{
-				String jadCharset = "UTF-8";
 				if (isMsd) 
 				{ 
-					jadCharset = "CP949"; 
 					Mobile.isSKT = true; 
 					Mobile.textEncoding = "EUC_KR"; 
 					MobilePlatform.checkFileEncoding();
 				}
 
 				String preparedFileName = fileName.replace("file:", "").trim();
-				try { preparedFileName = URLDecoder.decode(preparedFileName, "UTF-8"); }
+				try { preparedFileName = URLDecoder.decode(preparedFileName, Mobile.textEncoding); }
 				catch (Exception e) 
 				{
 					System.err.println("Error decoding file name: " + e.getMessage());
@@ -747,7 +745,7 @@ public class MobilePlatform
 				try 
 				{
 					targetStream = new FileInputStream(preparedFileName);
-					try { MIDletLoader.parseDescriptorInto(targetStream, descriptorProperties, jadCharset); } 
+					try { MIDletLoader.parseDescriptorInto(targetStream, descriptorProperties); } 
 					finally { targetStream.close(); }
 				} 
 				catch (IOException e) 